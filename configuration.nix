1ccaa74230f3abe9d33f0b92b84664e520c48afe
Jack <jack@sparkpc.email>
2022-05-06 00:34:32 +0000

    Rm hardware config

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,7 @@ let jack-info = import ./jack-info.nix {inherit pkgs; } ; in
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
